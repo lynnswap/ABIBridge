4dd624fbe597cde14d115fa8b1b72952cd5fc7bf
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2026-09-22 19:22:24 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ABIBridge",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ABIBridge",
+            targets: ["ABIBridge"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ABIBridge",
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+        .testTarget(
+            name: "ABIBridgeTests",
+            dependencies: ["ABIBridge"],
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+    ]
+)
